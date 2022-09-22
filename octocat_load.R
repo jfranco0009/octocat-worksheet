61a93330e08549db1771ed43a06878b725c76f6a
Jeniah Franco <jfranco@smith.edujeniahfranco@Jeniahs-MacBook-Pro ~ % git config --global user.email jfranco@smith.edu>
2022-09-21 22:11:21 -0400

    fixed function

--- octocat_load.R
+++ octocat_load.R
@@ -1,6 +1,6 @@
 # Code to load in Octocat (github mascot) art
 
-octocat = readLines("./octocat.txt")
+octocat = readLines("https://raw.githubusercontent.com/Intro-to-Data-Science-Template/intro_to_data_science_reader/main/content/class_worksheets/6_git/octocat.txt")
 
 
 
