fad35bc903a9508a96158fc2204c26fb6e8c7ceb
Jeniah Franco <jfranco@smith.edujeniahfranco@Jeniahs-MacBook-Pro ~ % git config --global user.email jfranco@smith.edu>
2022-09-21 21:57:03 -0400

    replaced where we grabbed octocat data from (we now have it locally rather than on the internet)

--- octocat_load.R
+++ octocat_load.R
@@ -1,5 +1,7 @@
 # Code to load in Octocat (github mascot) art
 
-octocat = readLines("https://raw.githubusercontent.com/Intro-to-Data-Science-Template/intro_to_data_science_reader/main/content/class_worksheets/6_git/octocat.txt")
+octocat = readLines("./octocat.txt")
+
+
 
 writeLines(octocat, "./octocat.txt")
